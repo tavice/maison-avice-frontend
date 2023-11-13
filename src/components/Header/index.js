b3754584fa4cb30f09573380e7ecbf422e3d8151
tavice <thomasavice.ta@gmail.com>
2023-11-13 17:13:36 -0500

    update to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./styles.css";
 import logoImage from "../../assets/images/poppy_transparent.png";
+import logoImageMonogram from "../../assets/images/monogram_poppy_transparent.png";
+import poppyBackground from "../../assets/images/poppy_pattern_transparent.png";
 
 function Header() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -10,44 +12,49 @@ function Header() {
   };
 
   return (
-    <header className="header">
-      <div className="logo-image">
-        <img
-          src={logoImage}
-          alt="Maison Avice"
-        />
-      </div>
-
-      <div className="company-name">
-        <div className="company-name-h1">
-          <h1>MAISON AVICE</h1>
+    <header className="header" style={{ backgroundImage: `url(${poppyBackground})` }}>
+      <div className="nav-banner" >
+      <div className="logo-image-banner">
+          <img src={logoImageMonogram} alt="Maison Avice" />
         </div>
-        <div className="company-name-h2">
-          <h2>INTERIOR DESIGN</h2>
+        <button className="menu-button" onClick={toggleMenu}>
+          {isMenuOpen ? "X" : "☰"}
+        </button>
+
+        <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
+          <ul className="nav-list">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+
+            <li>
+              <button className="contact-button">Contact Us</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="logo-and-image" >
+        
+    
+        <div className="logo-image">
+          <img src={logoImage} alt="Maison Avice" />
+        </div>
+
+        <div className="company-name">
+          <div className="company-name-h1">
+            <h1>MAISON AVICE</h1>
+          </div>
+          <div className="company-name-h2">
+            <h2>INTERIOR DESIGN</h2>
+          </div>
         </div>
       </div>
-
-      <button className="menu-button" onClick={toggleMenu}>
-        {isMenuOpen ? "X" : "☰"}
-      </button>
-
-      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
-        <ul className="nav-list">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-
-          <li>
-            <button className="contact-button">Contact Us</button>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
