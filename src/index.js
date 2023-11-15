adbad300b347a9740c14c633876fc278e01156cc
tavice <thomasavice.ta@gmail.com>
2023-11-15 00:11:35 -0500

    added analytics

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+
+
 // import router
 import { BrowserRouter as Router } from "react-router-dom";
+
+//import vercel analytics
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,4 +26,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
